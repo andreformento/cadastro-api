b24e6bcde10cbb9dc09677b35bcebec42b18902e
André Formento <andreformento.sc@gmail.com>
2016-04-09 21:44:02 -0300

    filter

--- src/main/java/com/formento/cadastro/security/SpringSecurityConfig.java
+++ src/main/java/com/formento/cadastro/security/SpringSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -50,8 +49,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 // All other request need to be authenticated
                 .anyRequest().authenticated().and()
 
-                // Custom Token based authentication based on the header previously given to the client
-                .addFilterBefore(new StatelessAuthenticationFilter(tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class);
+        // Custom Token based authentication based on the header previously given to the client
+        //                .addFilterBefore(new StatelessAuthenticationFilter(tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class)
+        ;
     }
 
     @Override
