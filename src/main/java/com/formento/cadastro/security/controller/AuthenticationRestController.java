7843d4e4c407b717733999965c92a3228b2d1b36
André Formento <andreformento.sc@gmail.com>
2016-04-11 20:08:12 -0300

    clean code

--- src/main/java/com/formento/cadastro/security/controller/AuthenticationRestController.java
+++ src/main/java/com/formento/cadastro/security/controller/AuthenticationRestController.java
@@ -2,15 +2,11 @@ package com.formento.cadastro.security.controller;
 
 import com.formento.cadastro.model.Usuario;
 import com.formento.cadastro.security.JwtAuthenticationRequest;
-import com.formento.cadastro.security.component.JwtTokenUtil;
 import com.formento.cadastro.security.service.AuthenticationRestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,18 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationRestController {
 
-    @Value("${jwt.header}")
-    private String tokenHeader;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
     @Autowired
     private AuthenticationRestService authenticationRestService;
 
@@ -38,19 +22,5 @@ public class AuthenticationRestController {
     public ResponseEntity<Resource<Usuario>> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException {
         return ResponseEntity.ok(new Resource<>(authenticationRestService.createAuthenticationToken(authenticationRequest)));
     }
-
-//    @RequestMapping(value = "${jwt.route.authentication.refresh}", method = RequestMethod.GET)
-//    public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) {
-//        String token = request.getHeader(tokenHeader);
-//        String username = jwtTokenUtil.getEmailFromToken(token);
-//        JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
-//
-//        if (jwtTokenUtil.canTokenBeRefreshed(token, user.getUltimoLogin())) {
-//            String refreshedToken = jwtTokenUtil.refreshToken(token);
-//            return ResponseEntity.ok(new JwtAuthenticationResponse(refreshedToken));
-//        } else {
-//            return ResponseEntity.badRequest().body(null);
-//        }
-//    }
 
 }
