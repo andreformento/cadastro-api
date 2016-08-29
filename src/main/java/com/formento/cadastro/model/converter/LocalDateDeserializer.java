aa7fa9a7118a6123395af0ef99308ec4a3772aa4
andre <andre.formento@concrete.com.br>
2016-08-29 15:53:58 -0300

    update spring; test controller 100% coverage

---
+++ src/main/java/com/formento/cadastro/model/converter/LocalDateDeserializer.java
@@ -0,0 +1,20 @@
+package com.formento.cadastro.model.converter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.formento.cadastro.util.LocalDateUtil;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class LocalDateDeserializer extends JsonSerializer<LocalDate> {
+
+    @Override
+    public void serialize(LocalDate value, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException {
+        if (value != null) {
+            jsonGenerator.writeObject(LocalDateUtil.format(value));
+        }
+    }
+
+}
---
+++ src/main/java/com/formento/cadastro/model/converter/LocalDateTimeDeserializer.java
@@ -0,0 +1,20 @@
+package com.formento.cadastro.model.converter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.formento.cadastro.util.LocalDateTimeUtil;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeDeserializer extends JsonSerializer<LocalDateTime> {
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException {
+        if (value != null) {
+            jsonGenerator.writeObject(LocalDateTimeUtil.format(value));
+        }
+    }
+
+}
